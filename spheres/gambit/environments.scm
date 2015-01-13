0238edf16b0aa4fab967f2758d3b4498179076be
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2015-01-13 21:29:50 +0100

    improved environments

--- spheres/gambit/environments.scm
+++ spheres/gambit/environments.scm
@@ -7,8 +7,10 @@
            (extended-bindings)
            (not safe)
            (block))
-  (define-macro (check-arg pred val caller)
-    #!void)
+  (define-syntax check-arg
+    (syntax-rules ()
+      ((check-arg . ?any)
+       #!void)))
   (println "-- optimize environment --"))
  ;; Debug Environment
  (debug
@@ -17,13 +19,29 @@
            (debug-location)
            (debug-source)
            (debug-environments))
-  (define-macro (check-arg pred val caller)
-    `(let lp ((val ,val))
-       (if (,pred val)
-           val
-           (lp (error "Bad argument" val pred ,caller)))))
+  (define-syntax check-arg
+    (syntax-rules ()
+      ((check-arg ?pred ?val ?caller)
+       (let ((val ?val))
+         (if (?pred val)
+             val
+             (begin
+               (println (string-append "Value: " (object->string val)))
+               (println (string-append "Predicate: " (object->string '?pred)))
+               (error "Failed argument check in" '?caller)))))
+      ((check-arg ?pred ?val ?caller ?reason)
+       (let ((val ?val))
+         (if (?pred val)
+             val
+             (begin
+               (println (string-append "Value: " (object->string val)))
+               (println (string-append "Predicate: " (object->string '?pred)))
+               (println (string-append "Reason: " ?reason))
+               (error "Failed argument check in" '?caller)))))))
   (println "-- debug environment --"))
  ;; Null Environment
  (else
-  (define-macro (check-arg pred val caller)
-    #!void)))
+  (define-syntax check-arg
+    (syntax-rules ()
+      ((check-arg . ?any)
+       #!void)))))
